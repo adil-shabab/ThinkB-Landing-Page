d5006db57db4dc1a19124d9e4cc6c17f4bcc8b32
adil-shabab <adilshabab2004@gmail.com>
2024-12-03 18:15:26 +0530

    testimonial section added

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,19 @@ document.addEventListener("DOMContentLoaded", function () {
       slider.parentElement.appendChild(clone);
     });
   });
+
+  
+
+
+  $(document).ready(function () {
+    $('.customer-review-slider').slick({
+        dots: true,              // Enable navigation dots
+        infinite: true,          // Infinite loop
+        speed: 500,              // Transition speed
+        slidesToShow: 3,         // Show 1 slide at a time
+        slidesToScroll: 1,       // Scroll 1 slide at a time
+        autoplay: true,          // Enable autoplay
+        autoplaySpeed: 3000,     // Autoplay speed
+        arrows: false            // Disable navigation arrows
+    });
+});
